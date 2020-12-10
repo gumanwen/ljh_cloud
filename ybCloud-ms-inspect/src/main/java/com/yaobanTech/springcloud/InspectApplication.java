1a0b2767f356b6749a9ffd08641cf9ab1ea32628
lijiahao <lijh@yaobantech.com>
2020-12-10 17:17:47 +0800

    初始化

---
+++ ybCloud-ms-inspect/src/main/java/com/yaobanTech/springcloud/InspectApplication.java
@@ -0,0 +1,11 @@
+package com.yaobanTech.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InspectApplication.class, args);
+    }
+}
---
+++ ybCloud-ms-plan/src/main/java/com/yaobanTech/springcloud/PlanApplication.java
@@ -0,0 +1,11 @@
+package com.yaobanTech.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlanApplication.class, args);
+    }
+}
