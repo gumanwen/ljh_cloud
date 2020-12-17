7bc1c93f630fd2bc0cbbc3e1b2f9f3343be507ab
yuxy <yuxy@yaobanTech.com>
2020-12-17 16:35:59 +0800

    1

--- ybcloud-ms-plan-8002/src/main/java/com/yaobanTech/springcloud/controller/PlanController.java
+++ ybcloud-ms-plan-8002/src/main/java/com/yaobanTech/springcloud/controller/PlanController.java
@@ -1,6 +1,5 @@
 package com.yaobanTech.springcloud.controller;
 
-import com.yaobanTech.springcloud.service.RouteService;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,5 @@ import javax.annotation.Resource;
 @RestController
 @RefreshScope
 public class PlanController {
-     @Resource
-     private RouteService routeService;
 
-     @GetMapping("/get")
-     public String get(){
-          return routeService.helloworld();
-     }
 }
