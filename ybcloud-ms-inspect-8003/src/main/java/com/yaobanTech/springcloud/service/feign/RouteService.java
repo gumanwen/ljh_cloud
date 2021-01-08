4ebf52c4ccca6a4ffecc1863b8ab2b1c38754db7
lijiahao <lijh@yaobantech.com>
2021-01-08 17:55:59 +0800

    10号上线前

--- ybcloud-ms-inspect-8003/src/main/java/com/yaobanTech/springcloud/service/feign/RouteService.java
+++ ybcloud-ms-inspect-8003/src/main/java/com/yaobanTech/springcloud/service/feign/RouteService.java
@@ -4,6 +4,8 @@ package com.yaobanTech.springcloud.service.feign;
 import com.yaobanTech.springcloud.entity.utils.RespBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Map;
 
 @FeignClient(value ="routeService" )
@@ -15,6 +17,9 @@ public interface RouteService {
     @GetMapping("/api/route/signPoint/findList")
     RespBean findpoints(@RequestParam("routeId") Integer routeId);
 
+    @GetMapping("/api/route/signPoint/findListByTaskId")
+    RespBean findListByTaskId(@RequestParam("routeId") Integer routeId , @RequestParam("inspectTaskId") String inspectTaskId);
+
     @PostMapping("/api/route/signPoint/modify")
     RespBean updatePoint(@RequestBody Map<String, Object> params);
 
@@ -25,5 +30,11 @@ public interface RouteService {
     RespBean findSelection();
 
     @GetMapping("/api/route/signPoint/findSignedList")
-    RespBean findSignedList(@RequestParam("routeId") Integer routeId);
+    RespBean findSignedList(@RequestParam("routeId") Integer routeId,@RequestParam("taskId") String taskId);
+
+    @GetMapping("/api/route/way/findEnum")
+    RespBean findEnum(@RequestParam("code")String code);
+
+    @GetMapping("/api/route/signPoint/taskPoint")
+    RespBean taskPoint(@RequestParam("taskIds") List<String> taskIds, @RequestParam("routeId") Integer routeId);
 }
