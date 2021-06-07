d763754ce80db506b58ed65ad13511ef999587b0
yuxy <yuxy@yaobantech.com>
2021-06-07 18:29:45 +0800

    编号日期规则变更

--- ybcloud-ms-route-8001/src/main/java/com/yaobanTech/springcloud/service/impl/RedisService.java
+++ ybcloud-ms-route-8001/src/main/java/com/yaobanTech/springcloud/service/impl/RedisService.java
@@ -78,11 +78,11 @@ public class RedisService {
             Long id = null;
             Calendar calendar = Calendar.getInstance();
             if (hasExpire) {
-                calendar.set(Calendar.DAY_OF_MONTH,1);
-                calendar.set(Calendar.HOUR_OF_DAY, 0);
-                calendar.set(Calendar.MINUTE, 0);
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
+                calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+                calendar.set(Calendar.HOUR_OF_DAY, 23);
+                calendar.set(Calendar.MINUTE, 59);
+                calendar.set(Calendar.SECOND, 59);
+                calendar.set(Calendar.MILLISECOND, 9999);
                 date = calendar.getTime();
             } else {
                 calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE) + 10);
